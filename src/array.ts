a243c003e36b8a73fcd39184a8cc8b7ccf4f0b0c
Ju4nJ023 <jjaf2000@gmail.com>
2025-04-14 17:03:17 +0200

    commit 1

---
+++ src/array.ts
@@ -0,0 +1,168 @@
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
+
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
+
+
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+// a) Extraer pacientes asignados a Pediatría
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter(
+    (paciente) => paciente.especialidad === "Pediatra");
+};
+
+const pacientesPediatria = obtenPacientesAsignadosAPediatria(pacientes);
+console.log("Pacientes asignados a Pediatría:", pacientesPediatria);
+
+
+// b) Extraer pacientes asignados a Pediatría y menores de 10 años
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter((paciente) => paciente.especialidad === "Pediatra" && paciente.edad < 10);
+};
+const pacientesPediatriaMenoresDe10 = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(
+  pacientes
+);
+console.log(
+  "Pacientes asignados a Pediatría y menores de 10 años:",
+  pacientesPediatriaMenoresDe10
+);
+
+//apartado 2
+const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  return pacientes.some(
+    (paciente) =>
+      paciente.frecuenciaCardiaca > 100 && paciente.temperatura > 38.5
+  );
+  
+};
+
+const activarUrgencia = activarProtocoloUrgencia(pacientes);
+console.log("Activar protocolo de urgencia:", activarUrgencia);
+
+//apartado 3
+const reasignaPacientesAMedicoFamilia = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.map((paciente) => 
+    paciente.especialidad === "Pediatra" ? { ...paciente, especialidad:"Medico de familia" } : paciente
+  );
+
+};
+
+const pacientesReasignados = reasignaPacientesAMedicoFamilia(pacientes);
+console.log("Pacientes reasignados a Médico de familia:", pacientesReasignados);
+
+//apartado 4
+const HayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+ return pacientes.some(
+    (paciente) => paciente.especialidad === "Pediatra"
+ );
+};
+const hayPacientesPediatria = HayPacientesDePediatria(pacientes);
+console.log("¿Hay pacientes de pediatría?:", hayPacientesPediatria);
+
+interface NumeroPacientesPorEspecialidad {
+  medicoDeFamilia: number;
+  pediatria: number;
+  cardiologia: number;
+}
+//apartado 5
+const cuentaPacientesPorEspecialidad = (
+  pacientes: Pacientes[]
+): NumeroPacientesPorEspecialidad => {
+  return pacientes.reduce(
+    (conteo, pacientes) => {
+      switch (pacientes.especialidad) {
+        case "Medico de familia":
+          conteo.medicoDeFamilia++;
+          break;
+        case "Pediatra":
+          conteo.pediatria++;
+          break;
+        case "Cardiólogo":
+          conteo.cardiologia++;
+          break;
+      }
+      return conteo;
+    }
+    , 
+    { medicoDeFamilia: 0, pediatria: 0, cardiologia: 0 }
+  );
+};
+
+const conteoPacientes = cuentaPacientesPorEspecialidad(pacientes);
+console.log("Conteo de pacientes por especialidad:", conteoPacientes);
